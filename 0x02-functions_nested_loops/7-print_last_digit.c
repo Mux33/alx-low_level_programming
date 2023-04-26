898c6ec0481adcc4634bc0b4b2eb92ed7c4edd49
Mux33 <kami.muthoga@gmail.com>
2023-04-25 19:26:14 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,10 @@
  *
  * Return: last digit.
  */
-int main(void)
+int print_last_digit(int n)
 {
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	if (n < 0)
+		n = -n;
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
