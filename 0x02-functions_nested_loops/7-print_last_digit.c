7e155e73d9383bb2906aca6299a96bdefd063543
Mux33 <kami.muthoga@gmail.com>
2023-04-25 18:12:26 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,15 @@ int print_last_digit(int n)
 {
 	int l;
 
-	if (n < 0)
-	n = -n;
-
 	l = n % 10;
-
 	if (l < 0)
-	l = -l;
-
-	_putchar(l + "0');
-
-	return (l);
+	{
+		_putchar(-l + 98);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l +98);
+		return (l);
+	}
 }
